795b40196e35568d617ec6d3b83c8c7973a54937
vikesh dass <vikesh.dass@nagarro.com>
2025-06-02 12:36:01 +0000

    getting image upload failed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -6,7 +6,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCU6pDf2Il4rINo_ZTO1qBmPi5D_P0flJA",
   authDomain: "ycompany-app.firebaseapp.com",
   projectId: "ycompany-app",
-  storageBucket: "ycompany-app.firebasestorage.app",
+  storageBucket: "ycompany-app.appspot.com", // Corrected format
   messagingSenderId: "931317445669",
   appId: "1:931317445669:web:00ed7eb9d5cb005507bbcc",
   measurementId: "G-5X9VWV0839"
